a1573021299f5a252b2cf7ceaf2281ab4ca0f6de
Baptiste Lecocq <hello@tiste.io>
2014-08-08 00:18:13 +0100

    Build base

--- angular-loading-button.js
+++ angular-loading-button.js
@@ -10,8 +10,24 @@ angular.module('loadingButton', [])
       ) {
         return {
           restrict: 'E',
-          scope: {},
-          link: function(scope, element, attr) {}
+          transclude: true,
+          replace: true,
+          scope: {
+            fail: '=',
+            success: '=',
+            value: '='
+          },
+          template: '\
+            <div class="progress-button">\
+              <button><span ng-transclude></span></button>\
+              <svg class="progress-circle" width="70" height="70"><path d="m35,2.5c17.955803,0 32.5,14.544199 32.5,32.5c0,17.955803 -14.544197,32.5 -32.5,32.5c-17.955803,0 -32.5,-14.544197 -32.5,-32.5c0,-17.955801 14.544197,-32.5 32.5,-32.5z"/></svg>\
+            </div>\
+          ',
+          link: function(scope, element, attr) {
+            scope.$watch('value', function(value) {
+              $log.info(value);
+            });
+          }
         };
       }
     ]
